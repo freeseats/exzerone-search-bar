ec6b2d1a7134185d92f59e0b6bdb5a863f8d558f
Chris Huang <chrishuang128@gmail.com>
2019-05-31 16:35:32 -0700

    fixed port issue for deploying

--- client/src/components/Searchbox.jsx
+++ client/src/components/Searchbox.jsx
@@ -40,7 +40,7 @@ class Searchbox extends React.Component {
 	}
 
 	componentDidMount() {
-		Axios.get('http://localhost:3030/restaurants')
+		Axios.get('http://54.226.159.194:80/restaurants')
 			.then((result) => {
 				const { data } = result;
 				const locations = [];
